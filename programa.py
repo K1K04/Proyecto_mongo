d091f21e869f720a13907ed155e6d70211260962
kiko <95848578+K1K04@users.noreply.github.com>
2024-05-08 20:48:07 +0200

    Update programa.py

--- programa.py
+++ programa.py
@@ -30,6 +30,11 @@ def update_movie():
         print("No se encontró ninguna película con ese título.")
 
 def consultar_por_año():
+    años = db.movies.distinct("year")
+    print("Años disponibles:")
+    for año in años:
+        print(año)
+
     año = input("Introduce el año de la película que deseas consultar: ")
     peliculas = list(db.movies.find({"year": año}, {"_id": 0, "title": 1}))
     if not peliculas:
@@ -39,13 +44,57 @@ def consultar_por_año():
         for pelicula in peliculas:
             print(pelicula['title'])
 
+
+def consulta_por_actor():
+    actores = db.movies.distinct("actors")
+    print("Actores disponibles:")
+    for actor in actores:
+        print(actor)
+
+    actor = input("Introduce el nombre del actor que deseas buscar: ")
+    peliculas = list(db.movies.find({"actors": actor}, {"_id": 0, "title": 1}))
+    if not peliculas:
+        print("No se encontraron películas con ese actor.")
+    else:
+        print(f"Películas con el actor {actor}:")
+        for pelicula in peliculas:
+            print(pelicula['title'])
+
+def consulta_por_premio():
+    premios = db.movies.distinct("awards.name")
+    print("Premios disponibles:")
+    for premio in premios:
+        print(premio)
+
+    premio = input("Introduce el nombre del premio que deseas buscar: ")
+    peliculas = list(db.movies.find({"awards.name": premio}, {"_id": 0, "title": 1}))
+    if not peliculas:
+        print("No se encontraron películas con ese premio.")
+    else:
+        print(f"Películas que han ganado el premio {premio}:")
+        for pelicula in peliculas:
+            print(pelicula['title'])
+
+def consulta_agrupacion():
+    pipeline = [
+        {"$unwind": "$genres"},
+        {"$group": {"_id": "$genres", "count": {"$sum": 1}}}
+    ]
+    resultados = db.movies.aggregate(pipeline)
+    print("Número de películas por género:")
+    for resultado in resultados:
+        print(f"{resultado['_id']}: {resultado['count']}")
+
 while True:
     print("\nMenú:")
     print("1. Insertar película")
     print("2. Eliminar película")
     print("3. Actualizar película")
     print("4. Buscar película por año")
-    print("5. Salir")
+    print("5. Consultar películas por actor")
+    print("6. Consultar películas por premio")
+    print("7. Consulta de agrupación por género")
+    print("8. Salir")
 
     choice = input("Seleccione una opción: ")
 
@@ -58,6 +107,12 @@ while True:
     elif choice == "4":
         consultar_por_año()
     elif choice == "5":
+        consulta_por_actor()
+    elif choice == "6":
+        consulta_por_premio()
+    elif choice == "7":
+        consulta_agrupacion()
+    elif choice == "8":
         print("¡Hasta luego!")
         break
     else:
